ddc6ea221caae1bcd2cd91517958d7c259207533
euijeong20 <euijeong20@naver.com>
2025-03-17 01:09:21 +0900

    egg

--- Assets/Scripts/HEJ/Egg.cs
+++ Assets/Scripts/HEJ/Egg.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Egg : MonoBehaviour
 {
-    private float countdownTime = 10f; // 카운트다운 시간
-    public TextMeshProUGUI countdownText;
+    private float countdownTime = 3f; // 카운트다운 시간
+    //public TextMeshProUGUI countdownText;
     private bool isCountingDown = false; // 카운트다운 중인지 확인
+    [SerializeField] GameObject effect;
+    [SerializeField] GameObject cpSpider;
+    
 
     //private void OnCollisionEnter(Collision col)
     //{
@@ -15,6 +18,11 @@ public class Egg : MonoBehaviour
     //        StartCoroutine(StartCountdown());
     //    }
     //}
+
+    private void Start()
+    {
+        //egg = gameObject.GetComponent<GameObject>();
+    }
 
     private void OnTriggerEnter(Collider juju)
     {
@@ -31,16 +39,28 @@ public class Egg : MonoBehaviour
 
         while (timeLeft > 0)
         {
-            if (countdownText != null)
-                countdownText.text = timeLeft.ToString("F0");
+            Debug.Log(timeLeft.ToString("F0"));
 
             yield return new WaitForSeconds(1f);
             timeLeft--;
+        } 
+        if(timeLeft == 0)
+        {
+            StartCoroutine(EffectRoutine());
         }
 
-        if (countdownText != null)
-            countdownText.text = "0"; // 카운트다운 종료 표시
 
         isCountingDown = false; // 카운트다운 종료 후 다시 감지 가능
     }
+
+    IEnumerator EffectRoutine()
+    {
+        effect.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        gameObject.SetActive(false);
+        Instantiate(cpSpider);
+        Debug.Log("153");
+    }
+
+   
 }
