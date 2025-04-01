79552248fc86d60fb1034624ac81493bc846ff1d
jiyeong <kim2000212@gmail.com>
2025-04-01 16:50:18 +0900

    크리처2 순찰,공격, 추격기능구조 완

--- Assets/Scripts/KJY/Creature2Manager.cs
+++ Assets/Scripts/KJY/Creature2Manager.cs
@@ -1,66 +1,45 @@
-﻿//using UnityEngine;
-
-//public class Creature2Manager : MonoBehaviour
-//{
-//    //눈알 감지 횟수 
-//    private int totalDetectionCount = 0; 
-
-//    public void IncreaseDetectionCount()
-//    {
-//        //감지 횟수 증가
-//        totalDetectionCount++; 
-//        Debug.Log($"총 감지 횟수: {totalDetectionCount}");
-
-//        //감지 횟수에 따라 크리처 성장
-//        UpdateCreatureState();
-//    }
-
-//    private void UpdateCreatureState()
-//    {
-//        if (totalDetectionCount > 5)
-//        {
-//            Debug.Log("크리처가 강해진다");
-//            //크리처 성장 관련 로직 추가 (예: 속도 증가, 공격력 증가 등)
-//        }
-//    }
-
-//    //감지 횟수 가져오기
-//    public int GetDetectionCount() => totalDetectionCount;
-//}
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Creature2Manager : MonoBehaviour
+
 {
-    internal static object instance;
-    [SerializeField] private int detectionThreshold = 5; // 예시: 5번 감지되면 성장
-    private int totalDetectionCount = 0;
+    [Header("크리처 관련 설정")]
+    [SerializeField] private GameObject creature2;
+    [SerializeField] private int growThreshold = 3; // 몇 번 감지되면 성장하는지
 
-    // 크리처의 단계나 상태를 표현할 수 있음
-    private int creatureStage = 1;
 
-    public void IncreaseDetectionCount()
+    private float TheTime = 0;
+    private int detectionCount = 0;
+
+    public bool CanTeleportation = false;
+    public bool CanGrow = false;
+
+    public void OnPlayerDetected()
     {
-        totalDetectionCount++;
-        Debug.Log($"총 감지 횟수: {totalDetectionCount}");
+        //크리처를 감지된 3초후에 크리처 순간이동
+        //크리처가 순간이동하고 나서 횟수 증가
+        //만약에 회수 5번 이상이면 크리처 성장한다.
+        TheTime += Time.deltaTime;
+        Debug.Log($"시간 : {TheTime}");
+        Debug.Log($"플레이어 감지됨! 현재 감지 횟수: {detectionCount}");
 
-        CheckGrowth();
-    }
-
-    private void CheckGrowth()
-    {
-        if (totalDetectionCount >= detectionThreshold)
+        //2초되면 횟수 증가하고 
+        if(TheTime >= 0.3f)
         {
-            GrowCreature();
-            totalDetectionCount = 0; // 초기화하거나 누적 유지 선택 가능
+            //크리처 순간이동한다.
+            //횟수 추가한다.
+            CanTeleportation = true;
+            detectionCount++;
+            TheTime = 0;
+        }
+
+        if (detectionCount % growThreshold == 0)
+        {
+            CanGrow = true;
         }
     }
 
-    private void GrowCreature()
-    {
-        creatureStage++;
-        Debug.Log($"크리처가 성장! 현재 단계: {creatureStage}");
 
-        // 예시로 크리처 속도 증가 또는 외형 변경 가능
-        // 예: this.GetComponent<CreatureBehavior>().IncreaseSpeed();
-    }
+
+
 }
