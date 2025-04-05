1de2654ec68bea63df8ede3baa6781419cf80017
euijeong20 <euijeong20@naver.com>
2025-04-05 22:45:13 +0900

    UI

---
+++ Assets/Scripts/HEJ/UIManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene("");
+    } 
+    public void GameEnd()
+    {
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit();
+    #endif
+    }
+}
