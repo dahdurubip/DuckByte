df82ae811098110bf40b1383f399db71d7b82422
jiyeong <kim2000212@gmail.com>
2025-05-19 13:14:17 +0900

    방기믹들 완료 & 다른 방 꾸미기 진행중

--- Assets/Scripts/KJY/SceneLoader.cs
+++ Assets/Scripts/KJY/SceneLoader.cs
@@ -3,8 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadScene(string sceneName)
+    //public void LoadScene(string sceneName)
+    //{
+    //    SceneManager.LoadScene(sceneName);
+    //}
+
+
+    private void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene("Creature1Map");
     }
 }
