0e62c2058568b9acc9954a22da07ff34af90534e
DESKTOP-QNNOQTD\user <dndb0413@naver.com>
2025-05-19 13:40:18 +0900

    수정

--- Assets/Scripts/CDH/Statue.cs
+++ Assets/Scripts/CDH/Statue.cs
@@ -5,15 +5,21 @@ public class Statue : MonoBehaviour
 {
     public bool isLit = false;                    // 불이 켜졌는지
     public event Action<Statue> OnFireLit;        // 불 켤 때 호출할 이벤트
-    public GameObject fireEffect;             // 불 켤 때 나타날 파티클
+    //public GameObject fireEffect;             // 불 켤 때 나타날 파티클
+    public ParticleSystem fireEffect;
+
+    private void Awake()
+    {
+        fireEffect.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+    }
 
     public void LightFire()
     {
         if (isLit) return;
         isLit = true;
 
-        fireEffect.SetActive(true);
-        //fireEffect.Play(); // 파티클 재생
+        //fireEffect.SetActive(true);
+        fireEffect.Play(true); // 파티클 재생
 
 
         // 이펙트, 소리 등 추가 가능
@@ -28,7 +34,8 @@ public class Statue : MonoBehaviour
         // 불 끄는 이펙트/로직 넣기
         if (fireEffect != null)
         {
-            fireEffect.SetActive(false);
+            //fireEffect.SetActive(false);
+            fireEffect.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
         }
     }
 }
