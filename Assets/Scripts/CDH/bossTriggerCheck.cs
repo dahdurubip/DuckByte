f1869094e11318b04a33dc6d97466b684f725495
ROG\a1459 <a1459289526@gmail.com>
2025-07-04 21:04:01 +0900

    수정

--- Assets/Scripts/CDH/bossTriggerCheck.cs
+++ Assets/Scripts/CDH/bossTriggerCheck.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 public class bossTriggerCheck : MonoBehaviour
 {
     public GameObject bossTM;
-    public GameObject camera3;
+    //public GameObject camera3;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             bossTM.SetActive(true);
-            camera3.SetActive(true);
+            //camera3.SetActive(true);
         }
     }
 }
